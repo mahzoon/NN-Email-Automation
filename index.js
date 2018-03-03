1738eb4d52ad98d6cd32892bd89af7d5a216e1a1
mahzoon <mmahzoon@uncc.edu>
2018-03-03 02:20:38 -0500

    bug fix

--- index.js
+++ index.js
@@ -43,9 +43,8 @@ http.createServer(function(req, res) {
                                         
                                         // An object of options to indicate where to post to
                                         var postParams = {
-                                        host: 'https://us-central1-automated-email-client.cloudfunctions.net',
+                                        host: 'https://us-central1-automated-email-client.cloudfunctions.net/addEmailObservation',
                                         port: 443,
-                                        path: '/addEmailObservation',
                                         method: 'POST',
                                         headers: {
                                         'Content-Type': 'application/x-www-form-urlencoded',
