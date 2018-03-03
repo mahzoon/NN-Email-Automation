4623151328af8ec208b04d6289e9b3c644164275
mahzoon <mmahzoon@uncc.edu>
2018-03-03 02:05:30 -0500

    makes firebase request

--- index.js
+++ index.js
@@ -2,7 +2,11 @@
 
 
 var formidable = require('formidable'),
-    http = require('http');
+    http = require('http'),
+    cloudinary = require('cloudinary'),
+    fs = require('fs');
+
+cloudinary.config({ cloud_name: 'university-of-colorado' });
 
 http.createServer(function(req, res) {
     /* Process the form uploads */
@@ -11,10 +15,58 @@ http.createServer(function(req, res) {
     form.parse(req, function(err, fields, files) {
              res.writeHead(200, {'content-type': 'text/plain'});
              res.write('Done.');
-               console.log(fields);
-               console.log(files);
-             res.end();
+             
+             var filesList = [];
+             var counter = 0;
+             Object.keys(files).forEach(function(key) {
+                  var file = files[key];
+                  filesList.push(file.path);
+                                        
+//                  cloudinary.uploader.unsigned_upload(file.path, 'web-preset', function(result) {
+//                                                      console.log(result)
+//                                                      fs.unlink(file.path, (err) => {
+//                                                                if (err) console.log(err);
+//                                                                });
+//                                                      });
+                                        
+                                        // make firebase request to add the email observation
+                                        
+                                        var observation = querystring.stringify({
+                                        'id' : counter,
+                                        'from': fields['from'],
+                                        'subject': fields['subject'],
+                                        'body-plain' : fields['body-plain'],
+                                        'timestamp' : fields['timestamp'],
+                                        'attachment': file.path
+                                        });
+                                        
+                                        // An object of options to indicate where to post to
+                                        var postParams = {
+                                        host: 'https://us-central1-automated-email-client.cloudfunctions.net/',
+                                        path: '/addEmailObservation',
+                                        method: 'POST',
+                                        headers: {
+                                        'Content-Type': 'application/x-www-form-urlencoded',
+                                        'Content-Length': Buffer.byteLength(observation)
+                                        }
+                                        };
+                                        
+                                        // Set up the request
+                                        var postReq = http.request(postParams, function(res) {
+                                                                    res.setEncoding('utf8');
+                                        });
+                                        
+                                        // post the data
+                                        postReq.write(observation);
+                                        postReq.end();
+                                        
+                                        counter = counter + 1;
+                                        
              });
+             console.log(filesList);
+               
+             res.end();
+    });
 
     form.on('error', function(err) {
           console.error(err);
