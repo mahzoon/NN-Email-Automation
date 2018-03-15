fdb22b361a857f674921d4808926d053d9d35011
mahzoon <mmahzoon@uncc.edu>
2018-03-15 08:20:11 -0400

    sending observation even when no attachments

--- index.js
+++ index.js
@@ -24,50 +24,23 @@ http.createServer(function(req, res) {
                   cloudinary.uploader.unsigned_upload(file.path, 'web-preset', function(result) {
                                         
                         // make firebase request to add the email observation
-                        
-                        var observation = querystring.stringify({
-                        'id' : counter,
-                        'from': fields['from'],
-                        'subject': fields['subject'],
-                        'body-plain' : fields['body-plain'],
-                        'timestamp' : fields['timestamp'],
-                        'attachment': result['secure_url']
-                        });
-                        
-                        // An object of options to indicate where to post to
-                        var postParams = {
-                        host: 'us-central1-automated-email-client.cloudfunctions.net',
-                        path: '/addEmailObservation',
-                        method: 'POST',
-                        headers: {
-                        'content-type': 'application/x-www-form-urlencoded',
-                        'Content-Length': observation.length
-                        }
-                        };
-                        
-                        // Set up the request
-                        var postReq = http.request(postParams, function(res) {
-                            res.setEncoding('utf8');
-                            res.on('data', function (chunk) {});
-                            res.on('end', function () {});
-                        });
-                        
-                        postReq.on('error', (e) => {
-                            console.error(`problem with request: ${e.message}`);
-                        });
-                        
-                        // post the data
-                        postReq.write(observation);
-                        postReq.end();
+                        sendEmailObservation(counter, fields['from'], fields['subject'],
+                                             fields['body-plain'], fields['timestamp'],
+                                             result['secure_url']);
                         
                         // delete the temp file
                         fs.unlink(file.path, (err) => { if (err) console.log(err); });
 
                         counter = counter + 1;
                 });
-                                        
              });
-               
+             
+             if (Object.keys(files).length == 0) {
+                // the email came without attachment
+                sendEmailObservation(0, fields['from'], fields['subject'],
+                                     fields['body-plain'], fields['timestamp'], '');
+             }
+            
              res.end();
     });
 
@@ -86,3 +59,42 @@ http.createServer(function(req, res) {
     res.end();
 }).listen((process.env.PORT || 5000));
 
+
+function sendEmailObservation(id, from, subject, body, timestamp, attachment) {
+    // make firebase request to add the email observation
+    
+    var observation = querystring.stringify({
+                                            'id' : id,
+                                            'from': from,
+                                            'subject': subject,
+                                            'body-plain' : body,
+                                            'timestamp' : timestamp,
+                                            'attachment': attachment
+                                            });
+    
+    // An object of options to indicate where to post to
+    var postParams = {
+    host: 'us-central1-automated-email-client.cloudfunctions.net',
+    path: '/addEmailObservation',
+    method: 'POST',
+    headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'Content-Length': observation.length
+    }
+    };
+    
+    // Set up the request
+    var postReq = http.request(postParams, function(res) {
+                               res.setEncoding('utf8');
+                               res.on('data', function (chunk) {});
+                               res.on('end', function () {});
+                               });
+    
+    postReq.on('error', (e) => {
+               console.error(`problem with request: ${e.message}`);
+               });
+    
+    // post the data
+    postReq.write(observation);
+    postReq.end();
+}
